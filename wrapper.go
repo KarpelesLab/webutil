fa712634f49e1fff33365e464b47fb860856d9a6
Mark Karpeles <magicaltux@gmail.com>
2021-11-18 13:22:46 +0900

    add shorthand Wrap() and WrapFunc()

--- wrapper.go
+++ wrapper.go
@@ -10,9 +10,22 @@ type Wrapper struct {
 	Child Handler
 }
 
+type WrapFunc func(w http.ResponseWriter, req *http.Request) error
+
 func (wrapper *Wrapper) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	err := wrapper.Child.ServeHTTP(w, req)
 	if err != nil {
 		ErrorToHttpHandler(err).ServeHTTP(w, req)
 	}
+}
+
+func (wf WrapFunc) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := wf(w, req)
+	if err != nil {
+		ErrorToHttpHandler(err).ServeHTTP(w, req)
+	}
+}
+
+func Wrap(h Handler) http.Handler {
+	return &Wrapper{h}
 }
