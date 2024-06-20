57fbb1d8385bfa0f0c2dd018ae22dd3e6e9ecf3f
Mark Karpeles <magicaltux@gmail.com>
2024-06-21 08:44:40 +0900

    simplify ParseIPPort

--- ipport.go
+++ ipport.go
@@ -3,65 +3,28 @@ package webutil
 import (
 	"net"
 	"strconv"
-	"strings"
 )
 
 // ParseIPPort will parse an IP with optionally a port
 func ParseIPPort(ip string) *net.TCPAddr {
-	if len(ip) < 2 {
+	host, port, err := net.SplitHostPort(ip)
+	if err != nil {
+		// fallback on ip only?
+		if ip := net.ParseIP(ip); ip != nil {
+			return &net.TCPAddr{IP: ip}
+		}
 		// can't parse something that small
 		return nil
 	}
-
-	res := &net.TCPAddr{}
-	pos := strings.LastIndex(ip, ":") // is there a port?
-
-	if ip[0] == '[' {
-		ip_end := strings.Index(ip, "]")
-		if ip_end == -1 {
-			return nil
-		}
-		if ip_end != pos-1 {
-			res.IP = net.ParseIP(ip[1 : len(ip)-1])
-			if res.IP != nil {
-				return res
-			}
-			return nil // :(
-		}
-
-		res.IP = net.ParseIP(ip[1:ip_end])
-		if res.IP == nil {
-			return nil
-		}
-		ip = ip[pos:]
-	} else if pos > 0 {
-		res.IP = net.ParseIP(ip[0:pos])
-		if res.IP == nil {
-			res.IP = net.ParseIP(ip)
-			if res.IP != nil {
-				return res
-			}
-			return nil
-		}
-		ip = ip[pos:]
-	} else if pos == -1 {
-		res.IP = net.ParseIP(ip)
-		if res.IP == nil {
-			return nil
-		}
-		return res
-	}
-
-	// only remains is ":port"
-	if ip[0] != ':' {
-		return nil
-	}
-
-	port, err := strconv.ParseUint(ip[1:], 10, 16)
+	portN, err := strconv.ParseUint(port, 10, 16)
 	if err != nil {
 		return nil
 	}
 
-	res.Port = int(port)
+	res := &net.TCPAddr{
+		IP:   net.ParseIP(host),
+		Port: int(portN),
+	}
+
 	return res
 }
